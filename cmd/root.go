5dcea0cb91e711e9289f3faa87ac67b76fdee8b3
Sina Zarei <sina.zarei.dev@gmail.com>
2025-02-23 15:33:42 -0500

    feat: add initial file structure
    Set up basic directory layout for project structure

---
+++ cmd/root.go
@@ -0,0 +1,41 @@
+/*
+Copyright © 2025 Sina Zarei sina.zarei.dev@gmail.com
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "qmars",
+	Short: "A CLI tool for generating QR codes, transferring files via QR codes, and converting data to QR codes.",
+	Long:  `qmars is a versatile command-line interface (CLI) tool. This application enables users to generate QR codes, transfer files between devices using QR codes, and convert various types of data into QR codes effortlessly.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.qmars.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2025 Sina Zarei sina.zarei.dev@gmail.com
+*/
+package main
+
+import "github.com/sudosz/qmars/cmd"
+
+func main() {
+	cmd.Execute()
+}
