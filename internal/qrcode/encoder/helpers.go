e085a48b9df364afc96d858d0adf2b925a02ea7e
Sina Zarei <sina.zarei.dev@gmail.com>
2025-02-25 19:01:04 -0500

    feat: add helper functions for QR code small string generation

--- internal/qrcode/encoder/helpers.go
+++ internal/qrcode/encoder/helpers.go
@@ -1,6 +1,8 @@
 package encoder
 
 import (
+	"strings"
+
 	"github.com/makiuchi-d/gozxing/qrcode/decoder"
 	"github.com/sudosz/qmars/internal/qrcode"
 )
@@ -17,4 +19,58 @@ func ecLevelToGozxingECLevel(l qrcode.ErrorCorrectionLevel) decoder.ErrorCorrect
 		return ecLevel
 	}
 	return decoder.ErrorCorrectionLevel_L
+}
+
+var (
+	smallChars = map[int]rune{
+		0:     ' ',
+		1:     '▀',
+		2:     '▄',
+		1 | 2: '█',
+	}
+	smallCharsInvert = map[int]rune{
+		0:     '█',
+		1:     '▄',
+		2:     '▀',
+		1 | 2: ' ',
+	}
+)
+
+func getCharOfBlockBools(bools ...bool) int {
+	idx := 0
+	for i, b := range bools {
+		if b {
+			idx |= 1 << i
+		}
+	}
+	return idx
+}
+
+func getQRCodeSmallestString(qr qrcode.QRCode, charset map[int]rune) string {
+	b := &strings.Builder{}
+	arr := qr.ToBoolArray()
+	h := len(arr)
+	hodd := h%2 == 1
+	for i := 0; i < h-h%2; i += 2 {
+		w := len(arr[i])
+		for j := 0; j < w; j++ {
+			b.WriteRune(charset[getCharOfBlockBools(arr[i][j], arr[i+1][j])])
+		}
+		b.WriteRune('\n')
+	}
+	if hodd {
+		for j := 0; j < len(arr[h-1]); j++ {
+			b.WriteRune(charset[getCharOfBlockBools(arr[h-1][j], false)])
+		}
+		b.WriteRune('\n')
+	}
+	return b.String()
+}
+
+func GetQRCodeSmallestString(qr qrcode.QRCode) string {
+	return getQRCodeSmallestString(qr, smallChars)
+}
+
+func GetQRCodeSmallestStringInvert(qr qrcode.QRCode) string {
+	return getQRCodeSmallestString(qr, smallCharsInvert)
 }
