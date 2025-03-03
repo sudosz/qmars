6496768bcbc9a0c4573283af5ce7790ce786b7d7
Sina Zarei <sina.zarei.dev@gmail.com>
2025-03-02 22:28:43 -0500

    fix: correct custom block filling for better scanner recognition
    - Fixed issue with QRCode images not being recognized by scanners.
    - Ensured position and alignment patterns are filled with simple color instead of image blocks.

--- internal/qrcode/qrcode.go
+++ internal/qrcode/qrcode.go
@@ -145,13 +145,50 @@ func (q customBlockQRCode) Bounds() image.Rectangle {
 func (q customBlockQRCode) At(x, y int) color.Color {
 	c := q.bg
 	realX, realY := x/q.b.Bounds().Dx(), y/q.b.Bounds().Dy()
-	if q.Get(realX, realY) {
-		c = q.b.At(x%q.b.Bounds().Dx(), y%q.b.Bounds().Dy())
+	ms := q.GetMarginSize()
+	r := image.Rect(ms, ms, q.GetWidth()-ms, q.GetHeight()-ms)
+	if image.Pt(realX, realY).In(r) {
+		if cl, ok := q.checkInPatterns(q.GetWidth()-2*ms, q.GetHeight()-2*ms, realX-ms, realY-ms); ok {
+			c = cl
+		} else if q.Get(realX, realY) {
+			c = q.b.At(x%q.b.Bounds().Dx(), y%q.b.Bounds().Dy())
+		}
 	}
 	if _, _, _, a := c.RGBA(); a == 0 {
 		return q.bg
 	}
 	return c
+}
+
+func (q customBlockQRCode) checkInPatterns(w, h, x, y int) (color.Color, bool) {
+	// Check timing patterns
+	if (x == w-9 || x == w-5) && (h-10 < y && y < h-4) ||
+		(y == h-9 || y == h-5) && (w-10 < x && x < w-4) ||
+		(x == w-7 && y == h-7) {
+		return q.GetForeground(), true
+	}
+
+	pt := image.Pt(x, y)
+	if pt.In(image.Rect(w-7, h-7, w, h)) {
+		return nil, false
+	}
+
+	// Check finder patterns
+	if (x < 7 || x >= w-7) && (y < 7 || y >= h-7) {
+		if x == 0 || x == 6 || x == w-7 || x == w-1 ||
+			y == 0 || y == 6 || y == h-7 || y == h-1 {
+			return q.GetForeground(), true
+		}
+	}
+
+	// Check alignment patterns
+	if pt.In(image.Rect(2, 2, 5, 5)) ||
+		pt.In(image.Rect(w-5, 2, w-2, 5)) ||
+		pt.In(image.Rect(2, h-5, 5, h-2)) {
+		return q.GetForeground(), true
+	}
+
+	return nil, false
 }
 
 func (q QRCode) ToImageWithBlock(block image.Image) image.Image {
